9926ed64bcf2da4679a76275040484d5f5824117
bananablizzard <55606174+joshuasbee@users.noreply.github.com>
2020-03-20 12:01:55 -0600

    Added a todo in main activity

--- app/src/main/java/CS246_Group4/failsafe/MainActivity.java
+++ app/src/main/java/CS246_Group4/failsafe/MainActivity.java
@@ -7,10 +7,19 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Hash hasher = new Hash();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //hasher.getHash(password);
+        //TODO Add LOGIN function,
+        /* Will need to use hash function, to hash initial user password and save to
+        *  a file with an inconspicuous name, maybe "hashtag"
+        *  Then we will need to hash the user input to allow us to compare to saved hashed password,
+        *  and if it matches let them into the next activity, the accountlist.
+        * */
     }
 }
