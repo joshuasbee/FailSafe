3c650f48aaa0e0c77fcb6c701b26be84cb3744df
starwielder <seth1309@gmail.com>
2020-03-25 10:57:07 -0600

    Added class for accountList management

---
+++ app/src/main/java/CS246_Group4/failsafe/AccountList.java
@@ -0,0 +1,25 @@
+package CS246_Group4.failsafe;
+
+import com.google.common.collect.Table;
+
+import java.util.ArrayList;
+
+public class AccountList {
+    ArrayList<Account> accountList = new ArrayList<Account>(); // Create an ArrayList object
+
+
+    public void setAccountList(ArrayList<Account> accountList) {
+        this.accountList = accountList;
+    }
+
+    public ArrayList<Account> getAccountList() {
+        return accountList;
+    }
+
+    public void populateTable() {
+        for (Account element : accountList) {
+            System.out.println(element);
+            //Table.add(element);                          //Temporary placeholder until real exists
+        }
+    }
+}
